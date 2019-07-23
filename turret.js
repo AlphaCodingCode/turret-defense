6150fb55320e8cc721f0a06c3d1d8fd8bb6026cd
AlphaCodingcode <dilshan@alphacoding.com>
2019-07-23 14:50:39 -0400

    Class inheritance for turrets
    changed from one class for multiple turrets

--- turret.js
+++ turret.js
@@ -1,13 +1,11 @@
 class Turret {
-    constructor (x, y, img, type) {
+    constructor (x, y, img) {
         this.attached = true;
         this.x = x;
         this.y = y;
-        this.type = type;
         this.img = img;
         this.bullets = [];
         this.cooldown = 0;
-        this.cdMax = 60;
     }
 
     updateBasicTurret(deltaX, deltaY) {
@@ -20,6 +18,7 @@ class Turret {
         // update the turret's bullet locations
         if (this.bullets.length > 0) {
             for (let i = this.bullets.length - 1; i >= 0; i--) {
+                // splice bullets which are off screen
                 if (this.bullets[i].x < -10 || this.bullets[i].x > width || this.bullets[i].y < -10 || this.bullets[i].y > height) {
                     this.bullets.splice(i, 1);
                 } else {
@@ -36,25 +35,36 @@ class Turret {
         this.cooldown--;
     }
 
-    update() {
-        if (this.type == 1) {
-            // left turret
-            this.updateBasicTurret(-4, 0);
-        } else if (this.type == 2) {
-            // up turret
-            this.updateBasicTurret(0, -4);
-        }
-    }
-
-    render() {
+    render(bulletWidth) {
         // draw turret's bullets
         for (let i = 0; i < this.bullets.length; i++) {
-            ellipse(this.bullets[i].x, this.bullets[i].y, 16, 16);
+            ellipse(this.bullets[i].x, this.bullets[i].y, bulletWidth, bulletWidth);
         }
 
         // draw the actual turret
         image(this.img, this.x, this.y, 32, 32);
+    }
+}
 
+
+class VerticalTurret extends Turret {
+    constructor (x, y, img) {
+        super(x, y, img);
+        this.cdMax = 40;
     }
 
+    update() {
+        super.updateBasicTurret(0, -4);
+    }
+}
+
+class HorizontalTurret extends Turret {
+    constructor (x, y, img) {
+        super(x, y, img);
+        this.cdMax = 60;
+    }
+
+    update() {
+        super.updateBasicTurret(-4, 0);
+    }
 }
