5c193ce80850c872fd48ec5f030ca309b7ac857c
AlphaCodingcode <dilshan@alphacoding.com>
2019-07-24 11:45:42 -0400

    Minion class inheritance for slime

--- minion.js
+++ minion.js
@@ -1,38 +1,42 @@
 class Minion {
     constructor (x, y, img) {
-        this.x = x + 8;
-        this.y = y + 8;
+        this.x = (x * tileset.tileW) + 8;
+        this.y = (y * tileset.tileH) + 8;
         this.img = img;
         this.frame = 0;
-        this.frameCDs = [4, 4, 4, 4];
+        this.frameCDs = [4, 4, 4, 4]; // how many frames to wait before cycling to the next frame
         this.frameCD = this.frameCDs[0];
         this.health = 2;
         this.path = tileset.shortestPath(tileset.map[round(this.y / tileset.tileH)][round(this.x / tileset.tileW)], tileset.map[10][16]);
         this.pathIndex = 0;
         this.speed = 0.5;
+        this.sizeDisplacement = 8;
+        this.arrived = false;
     }
 
-    update() {
+    update(displacement) {
         // update the minion's position along the calc'd path
         // if the minion is within 8 pixels of the path, time to move on to the next index
-        if (dist(this.x - 8, this.y - 8, this.path[this.pathIndex].x, this.path[this.pathIndex].y) <= 8) {
+        if (dist(this.x - displacement, this.y - displacement, this.path[this.pathIndex].x, this.path[this.pathIndex].y) <= displacement) {
             this.pathIndex++;
             if (this.pathIndex == this.path.length) {
                 let temp = [];
                 temp.push(this.path[this.path.length - 1]);
                 this.path = temp;
                 this.pathIndex = 0;
+                // the minion has reached it's goal
+                this.arrived = true;
             }
         }
         // head towards the path index specified
-        if (this.x - 8 > this.path[this.pathIndex].x) {
+        if (this.x - displacement > this.path[this.pathIndex].x) {
             this.x -= this.speed;
-        } else if (this.x - 8 < this.path[this.pathIndex].x) {
+        } else if (this.x - displacement < this.path[this.pathIndex].x) {
             this.x += this.speed;
         }
-        if (this.y - 8 > this.path[this.pathIndex].y) {
+        if (this.y - displacement > this.path[this.pathIndex].y) {
             this.y -= this.speed;
-        } else if (this.y - 8 < this.path[this.pathIndex].y) {
+        } else if (this.y - displacement < this.path[this.pathIndex].y) {
             this.y += this.speed;
         }
 
@@ -44,13 +48,41 @@ class Minion {
             this.frameCD = this.frameCDs[this.frame];
         }
         this.frameCD--;
-        // if minion reaches it's goal, we should lose a heart
+    }
+
+    attacked(dmg = 1) {
+        if ((this.health - dmg) > 0) {
+            this.health -= dmg;
+        } else {
+            // the unit is killed
+            this.health = 0;
+        }
+    }
+
+    render(size) {
+        // draw the minion
+        imageMode(CORNER);
+        image(this.img, this.x, this.y, size, size, size * this.frame, 0, size, size);
+    }
+
+}
+
+class Slime extends Minion {
+    constructor (x, y, img) {
+        super(x, y, img);
+        this.frameCDs = [4, 4, 4, 4]; // how many frames to wait before cycling to the next frame
+        this.frameCD = this.frameCDs[0];
+        this.health = 2;
+        this.speed = 0.5; // half a pixel per frame
+        this.sizeDisplacement = 8; // how much to offset the image so it's centered on the tile
+        this.size = 16;
+    }
+
+    update() {
+        super.update(this.sizeDisplacement);
     }
 
     render() {
-        // draw the minion
-        imageMode(CORNER);
-        image(this.img, this.x, this.y, 16, 16, 16 * this.frame, 0, 16, 16);
+        super.render(this.size);
     }
-
 }
